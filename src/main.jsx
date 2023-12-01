ab38be00176905eb76d45ffc564b5f117f511ef0
jimmybombimmy <jamespeters92@gmail.com>
2023-12-01 18:12:58 +0000

    navbar 1st draft create

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Logo from './Logo.jsx'
 import IntervalExample from './IntervalExample.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Logo /> */}
     <App />
     {/* <IntervalExample /> */}
   </React.StrictMode>,
