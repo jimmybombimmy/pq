feb31ad00881d3e678ca21897eb41cef083843d0
jimmybombimmy <jamespeters92@gmail.com>
2023-12-08 00:31:47 +0000

    change to base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pq/"
+  base: "https://jimmybombimmy.github.io/pq"
 })
