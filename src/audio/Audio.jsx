cce679eaf9e3a520ba231e8eff94842f74e1dfcd
jimmybombimmy <jamespeters92@gmail.com>
2023-12-16 18:38:28 +0000

    part-complete responsive layout for audio grid

--- src/audio/Audio.jsx
+++ src/audio/Audio.jsx
@@ -5,6 +5,7 @@ function Audio() {
   const audioInfo = [
     {
       url: "https://www.fredsmithxmastrees.com/wp-content/uploads/2017/04/Square-500x500-dark-grey-300x300.png",
+      size: [2, 2],
     },
     {
       url: "https://www.fredsmithxmastrees.com/wp-content/uploads/2017/04/Square-500x500-dark-grey-300x300.png",
@@ -50,31 +51,33 @@ function Audio() {
   return (
     <main className="audioPageFull">
       <section className="audioContainer">
-      <section className="audioSection">
-        <div className="audioGridSection">
-          <h1 className="audioTitle">audio editing / dialogue</h1>
-          <p className="audioInfo">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Praesentium obcaecati itaque deserunt sit asperiores quam iure earum
-            iusto placeat possimus. Quae assumenda esse, eligendi quasi
-            doloribus hic perspiciatis explicabo ipsa similique, qui architecto
-            aliquam consequuntur nobis, a in quibusdam accusantium autem! Nam
-            facere voluptatibus nostrum incidunt tempora ipsa impedit cumque.
-          </p>
-          <img
-            className="audioImg audioImgLarge"
-            src="https://www.fredsmithxmastrees.com/wp-content/uploads/2017/04/Square-500x500-dark-grey-300x300.png"
-          ></img>
-          {audioInfo.map(function callback(value, index) {
-            return (
-              <img
-                className={`audioImg audioImg${index + 1}`}
-                src={value.url}
-                key={`audioImg${index + 1}`}
-              ></img>
-            );
-          })}
-        </div>
+        <section className="audioSection">
+          <div className="audioGridSection">
+            <h1 className="audioTitle">audio editing / dialogue</h1>
+            <p className="audioInfo">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Praesentium obcaecati itaque deserunt sit asperiores quam iure
+              earum iusto placeat possimus. Quae assumenda esse, eligendi quasi
+              doloribus hic perspiciatis explicabo ipsa similique, qui
+              architecto aliquam consequuntur nobis, a in quibusdam accusantium
+              autem! Nam facere voluptatibus nostrum incidunt tempora ipsa
+              impedit cumque.
+            </p>
+            <img
+              className="audioImg audioImgLarge"
+              src="https://www.fredsmithxmastrees.com/wp-content/uploads/2017/04/Square-500x500-dark-grey-300x300.png"
+              key="audioImgLarge"
+            ></img>
+            {audioInfo.map(function callback(value, index) {
+              return (
+                <img
+                  className={`audioImg audioImg${index + 1}`}
+                  src={value.url}
+                  key={`audioImg${index + 1}`}
+                ></img>
+              );
+            })}
+          </div>
         </section>
       </section>
     </main>
