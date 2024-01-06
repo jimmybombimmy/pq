b50332b9b045a895f93042cd3938ae07b2ead38d
jimmybombimmy <jamespeters92@gmail.com>
2024-01-06 21:59:30 +0000

    add production data into separate file and dynamically add it to the page

--- src/data/production-data.js
+++ src/data/production-data.js
@@ -1,3 +1,123 @@
 export const productionData = [
-  {artist }
-]
+  {
+    section: `full production | recording`,
+    releases: [
+      {
+        releaseName: "source of denial",
+        artistName: "nihiloxica",
+        img: "https://f4.bcbits.com/img/a3719623315_16.jpg",
+        desc: "all our music is recorded, synthed, produced and mixed by yours truly. most often from the nyege studios in kampala",
+        link: "https://nihiloxica.bandcamp.com/album/source-of-denial",
+      },
+      {
+        releaseName: "kaloli",
+        artistName: "nihiloxica",
+        img: "https://f4.bcbits.com/img/a2639495140_16.jpg",
+        desc: "this one was co-engineered with ross halden at hohm studios in bradford",
+        link: "https://nihiloxica.bandcamp.com/album/kaloli",
+      },
+      {
+        releaseName: "biliri",
+        artistName: "nihiloxica",
+        img: "https://f4.bcbits.com/img/a1977416058_16.jpg",
+        desc: "our second ep, recorded in the new (at the time) nyege nyege studios",
+        link: "https://nyegenyegetapes.bandcamp.com/album/biiri",
+      },
+      {
+        releaseName: "nihiloxica",
+        artistName: "nihiloxica",
+        img: "https://f4.bcbits.com/img/a1326165372_16.jpg",
+        desc: "co-engineered with ben beheshty at the original nyege nyege studios",
+        link: "https://nyegenyegetapes.bandcamp.com/album/nihiloxica",
+      },
+      {
+        releaseName: "Nakibembe Embaire Group",
+        artistName: "Nakibembe Embaire Group",
+        img: "https://f4.bcbits.com/img/a1332237328_16.jpg",
+        desc: "recorded this amazing embaire and its 6 interlocking players in their village in uganda with emiliano motta and jacob maskell-key. our recordings were used for tracks 4, 5, and 7.",
+        link: "https://nyegenyegetapes.bandcamp.com/album/nakibembe-embaire-group",
+      },
+      {
+        releaseName: "Ejokawulida",
+        artistName: "Nilotika Cultural Ensemble",
+        img: "https://f4.bcbits.com/img/a2515331861_16.jpg",
+        desc: "co-engineered with ben beheshty at the original nyege nyege studios",
+        link: "https://nyegenyegetapes.bandcamp.com/album/ejokawulida",
+      },
+    ],
+  },
+  {
+    section: `mixing`,
+    releases: [
+      {
+        releaseName: "Mubashira Mataali Group REPRESS (BLIP007)",
+        artistName: "Mubashira Mataali Group",
+        img: "https://f4.bcbits.com/img/a2723883060_16.jpg",
+        desc: "my first connection with ugandan music, courtesy of blip discs. mixed this with additional sub harmonisation for extra club impact.",
+        link: "https://blipdiscs.bandcamp.com/album/mubashira-mataali-group-repress-blip007",
+      },
+      {
+        releaseName: "funktion banned",
+        artistName: "hot in da club",
+        img: "https://f4.bcbits.com/img/a3339996242_16.jpg",
+        desc: "an involved mixing / arranging / editing job making creative decisions to bring the best out of live improv performances",
+        link: "https://spookyshit.bandcamp.com/album/funktion-banned",
+      },
+      {
+        releaseName: "avant harde",
+        artistName: "Hot in da club",
+        img: "https://f4.bcbits.com/img/a0936869761_16.jpg",
+        desc: "an involved mixing / arranging / editing job making creative decisions to bring the best out of live improv performances",
+        link: "https://spookyshit.bandcamp.com/album/avant-harde",
+      },
+      {
+        releaseName: "Mediokore",
+        artistName: "Hot in da club",
+        img: "https://f4.bcbits.com/img/a1349111703_16.jpg",
+        desc: "an involved mixing / arranging / editing job making creative decisions to bring the best out of live improv performances",
+        link: "https://spookyshit.bandcamp.com/album/mediokore",
+      },
+      {
+        releaseName: "not in da club",
+        artistName: "not in da club",
+        img: "https://f4.bcbits.com/img/a1554659250_16.jpg",
+        desc: "an involved mixing / arranging / editing job making creative decisions to bring the best out of live improv performances",
+        link: "https://opaltapes.bandcamp.com/album/not-in-da-club",
+      },
+      {
+        releaseName: "plucking the moon from the bottom of the ocean",
+        artistName: "capua collective",
+        img: "https://f4.bcbits.com/img/a3595780500_16.jpg",
+        desc: "also mastered by me",
+        link: "https://capua.bandcamp.com/album/plucking-the-moon-from-the-bottom-of-the-ocean",
+      },
+    ],
+  },
+  {
+    section: `mastering`,
+    releases: [
+      {
+        releaseName: "mass deflate",
+        artistName: "spooky-j ",
+        img: "https://f4.bcbits.com/img/a1496469512_16.jpg",
+        desc: "i’ve taken to mastering some of the spooky shit back catalogue after doing a blind test between our previous mastering engineer and my own contribution.",
+        link: "https://spookyshit.bandcamp.com/album/mass-deflate",
+      },
+      {
+        releaseName: "exhaust loop",
+        artistName: "pq",
+        img: "https://f4.bcbits.com/img/a0135970471_16.jpg",
+        desc: "first time mastering my own music for release - not something i like to do always!",
+        link: "https://spookyshit.bandcamp.com/album/exhaust-loop",
+      },
+      {
+        releaseName: "lost his shit",
+        artistName: "ekhe",
+        img: "https://f4.bcbits.com/img/a0945105307_16.jpg",
+        desc: "mastering unfinished demos is a challenging prospect, but i’d like to think i managed to help this wonderful set of tunes reach their best given the circumstances",
+        link: "https://spookyshit.bandcamp.com/album/lost-his-shit",
+      },
+    ],
+  },
+  
+];
--- src/production/Production.jsx
+++ src/production/Production.jsx
@@ -1,459 +1,62 @@
 import "../styles/App.css";
 import "../styles/Production.css";
-import { useState } from "react";
-import Collapse from "react-bootstrap/Collapse";
+import { productionData } from "../data/production-data";
 
 function Production() {
-  const [openProdInfo, setOpenProdInfo] = useState(false);
-
+  
   return (
-    <main className="prodPageFull">
-      <section>
-        <h1
-          className="prodTitle"
-          onClick={() => setOpenProdInfo(!openProdInfo)}
-          aria-controls="audioInfoDropdown"
-          aria-expanded={openProdInfo}
-        >
-          recording <span className="h1Slash">/</span> mixing <span className="h1Slash">/</span> mastering{" "}
-          <span className="plusMinus">{openProdInfo ? "-" : "+"}</span>
-        </h1>
-        <Collapse in={openProdInfo}>
-          <article className="prodInfoDropdown">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas,
-            doloribus unde mollitia saepe harum nam, corrupti pariatur deserunt
-            voluptate dignissimos ad! Dolor, ipsa quam ratione quis reiciendis
-            qui porro maiores labore saepe, aut itaque! Omnis, iste! Blanditiis,
-            maxime labore similique atque incidunt quae fugit voluptate
-            recusandae nemo quam assumenda quos maiores perferendis enim magni.
-            Ducimus, alias culpa eaque minima consequuntur reprehenderit
-            repellat labore soluta omnis cum, excepturi nulla ad veniam? Numquam
-            iste excepturi tempora dolorem sit voluptatibus, quis, quos
-            repudiandae inventore maiores earum eum consectetur amet mollitia
-            necessitatibus ea iusto incidunt molestiae aliquid omnis praesentium
-            repellat pariatur. Iste, est unde.
-          </article>
-        </Collapse>
-      </section>
-      <section className="recCont prodContainer">
-        <h2 className="prodSectionTitle">full production <span className="h2Slash">/</span> recording</h2>
-        <div className="prodGridSection">
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a3719623315_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a3719623315_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>source of denial</strong> by{" "}
-                      <strong>nihiloxica</strong>
-                    </u>
-                  </p>
-                  <p>
-                    Produced by Spooky-J and pq at Nyege Nyege HQ in Kampala.
-                  </p>
-                  <p>Mixed by pq</p>
-                  <p>Mastered by Arthur Rizk</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://nihiloxica.bandcamp.com/album/source-of-denial"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
+    <>
+      <main className="prodPageFull">
+        {productionData.map((group) => {
+          return (
+            <section className="recCont prodContainer">
+              <h2 className="prodSectionTitle">{group.section}</h2>
+              <div className="prodGridSection">
+                {group.releases.map((release) => {
+                  return (
+                    <div flipbox className="flipbox">
+                      <div className="flipboxInner">
+                        <div className="flipboxFront"></div>
+                        <img
+                          className="flipboxFrontImg"
+                          src={release.img}
+                          alt=""
+                        />
+                        <div className="flipboxBack">
+                          <img
+                            className="flipboxBackImg"
+                            src={release.img}
+                            alt=""
+                          />
+                          <article className="flipboxBackText whiteText">
+                            <p className="flipboxBackTitle">
+                              <u>
+                                <strong>{release.releaseName}</strong> by{" "}
+                                <strong>{release.artistName}</strong>
+                              </u>
+                            </p>
+                            <p>{release.desc}</p>
+                            <p>
+                              <a
+                                className="cleanLink"
+                                href={release.link}
+                                target="_blank"
+                              >
+                                Click here to listen
+                              </a>
+                            </p>
+                          </article>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a1332237328_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a1332237328_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Nakibembe Embaire Group</strong>
-                    </u>
-                  </p>
-                  <p>
-                    Track 2, 4, 5 & 7 Recorded by PQ, E.Motta & J. Maskell-Key.
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://nyegenyegetapes.bandcamp.com/album/nakibembe-embaire-group"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a2515331861_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a2515331861_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Ejokawulida</strong> by{" "}
-                      <strong>Nilotika Cultural Ensemble</strong>
-                    </u>
-                  </p>
-                  <p>Mixed by pq</p>
-                  <p>Mastered at Declared Sound</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://nyegenyegetapes.bandcamp.com/album/ejokawulida"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="mixingCont prodContainer">
-        <h2 className="prodSectionTitle">mixing</h2>
-        <div className="prodGridSection">
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a2723883060_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a2723883060_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>blip007 repress</strong> by{" "}
-                      <strong>Mubashira Mataali Group</strong>
-                    </u>
-                  </p>
-                  <p>
-                    Engineered by Jude from the National Theatre, Kampala and
-                    Blip Discs
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://blipdiscs.bandcamp.com/album/mubashira-mataali-group-repress-blip007"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a3339996242_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a3339996242_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Funktion Banned</strong> by{" "}
-                      <strong>Hot in da club</strong>
-                    </u>
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>Mastered by PQ</p>
-                  <p>recorded in a hot club</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://spookyshit.bandcamp.com/album/funktion-banned"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a0936869761_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a0936869761_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Avant-Harde</strong> by{" "}
-                      <strong>Hot in da club</strong>
-                    </u>
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>Mastered by PQ</p>
-                  <p>recorded in a hot club</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://spookyshit.bandcamp.com/album/avant-harde"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a1349111703_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a1349111703_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Mediokore</strong> by{" "}
-                      <strong>Hot in da club</strong>
-                    </u>
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>Mastered by PQ</p>
-                  <p>recorded in a hot club</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://spookyshit.bandcamp.com/album/mediokore"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="masteringCont prodContainer">
-        <h2 className="prodSectionTitle">mixing</h2>
-        <div className="prodGridSection">
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a1496469512_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a1496469512_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Mass Deflate</strong> by{" "}
-                      <strong>Spooky-J</strong>
-                    </u>
-                  </p>
-                  <p>Mastered by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://spookyshit.bandcamp.com/album/mass-deflate"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a0135970471_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a0135970471_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>exhaust loop</strong> by{" "}
-                      <strong>pq</strong>
-                    </u>
-                  </p>
-                  <p>Mixed by PQ</p>
-                  <p>Mastered by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://spookyshit.bandcamp.com/album/exhaust-loop"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://f4.bcbits.com/img/a0945105307_16.jpg"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://f4.bcbits.com/img/a0945105307_16.jpg"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>lost his shit</strong> by{" "}
-                      <strong>ekhe</strong>
-                    </u>
-                  </p>
-                  <p>Mastered by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://open.spotify.com/album/2Z9uZ1jLTsWj1M5rKMHr1X"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div flipbox className="flipbox">
-            <div className="flipboxInner">
-              <div className="flipboxFront"></div>
-              <img
-                className="flipboxFrontImg"
-                src="https://i.scdn.co/image/ab67616d0000b273eaaea9ec1409d680e6cb97f2"
-                alt=""
-              />
-              <div className="flipboxBack">
-                <img
-                  className="flipboxBackImg"
-                  src="https://i.scdn.co/image/ab67616d0000b273eaaea9ec1409d680e6cb97f2"
-                  alt=""
-                />
-                <article className="flipboxBackText whiteText">
-                  <p>
-                    <u>
-                      <strong>Plucking the Moon from the Bottom of the Ocean</strong> by{" "}
-                      <strong>Capua</strong>
-                    </u>
-                  </p>
-                  <p>Mastered by PQ</p>
-                  <p>
-                    <a
-                      className="cleanLink"
-                      href="https://open.spotify.com/album/2Z9uZ1jLTsWj1M5rKMHr1X"
-                      target="_blank"
-                    >
-                      Click here to listen
-                    </a>
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
+            </section>
+          );
+        })}
+      </main>
+    </>
   );
 }
 
